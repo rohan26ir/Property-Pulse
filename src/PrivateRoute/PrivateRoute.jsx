0bc219c3f4234ed1b90e05acc7aabd0f0f8818db
Rohan <rohan26ir@gmail.com>
2025-01-20 12:56:16 +0600

    add PrivateRoute and useAuth.jsx

---
+++ src/PrivateRoute/PrivateRoute.jsx
@@ -0,0 +1,21 @@
+import React, { useContext } from "react";
+import { Navigate, useLocation } from "react-router-dom";
+import { AuthContext } from "../Provider/Provider";
+import Loading from "../components/Loading";
+
+const PrivateRoute = ({ children }) => {
+  const { user, loading } = useContext(AuthContext);
+  const location = useLocation();
+
+  if (loading) {
+    return <Loading></Loading>;
+  }
+
+  if (user) {
+    return children;
+  }
+
+  return <Navigate state={location.pathname} to="/login" replace />;
+};
+
+export default PrivateRoute;

8ba534673ca95d97ca9598ab5e1c83642a817a92
Rohan <rohan26ir@gmail.com>
2025-01-20 19:50:21 +0600

    add About The Building in Home

---
+++ src/pages/Home/AboutBuilding/AboutBuilding.jsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from "react";
+
+const AboutBuilding = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("/aboutBuilding.json")
+      .then((res) => res.json()) // Fixed the missing parentheses in res.json()
+      .then((data) => setData(data))
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+
+  return (
+    <div className="p-6 ">
+      <div className="text-center mb-8">
+        <h2 className="text-3xl font-bold text-blue-600">About The Building</h2>
+        <p className="text-gray-700">
+          Explore details about our apartments and find your perfect match!
+        </p>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        {data.map((building) => (
+          <div
+            key={building._id}
+            className="bg-white shadow-md rounded-lg overflow-hidden"
+          >
+            <img
+              src={building.imageGallery[0]}
+              alt={building.name}
+              className="w-full h-48 object-cover"
+            />
+            <div className="p-4">
+              <h3 className="text-xl font-bold text-blue-800">{building.name}</h3>
+              <p className="text-gray-700 mt-2">{building.description}</p>
+              
+              <div className="mt-4 text-sm text-gray-500">
+                <p>
+                  <strong>Location:</strong> {building.location.address},{" "}
+                  {building.location.city}, {building.location.state},{" "}
+                  {building.location.postalCode}
+                </p>
+                
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default AboutBuilding;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Hero from './Hero';
+import AboutBuilding from './AboutBuilding/AboutBuilding';
+import Coupons from './Coupons/Coupons';
 
 const Home = () => {
   return (
@@ -7,6 +9,15 @@ const Home = () => {
       <header>
         <Hero></Hero>
       </header>
+
+      <main className='bg-gray-100'>
+        <div className='w-11/12 mx-auto py-5'>
+         <AboutBuilding></AboutBuilding>
+        </div>
+        <div>
+          <Coupons></Coupons>
+        </div>
+      </main>
     </div>
   );
 };
